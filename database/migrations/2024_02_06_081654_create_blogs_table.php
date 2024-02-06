25866bb216d467139e2f324e13ee059b0f3c293e
lalitharw <lalitharwate90@gmail.com>
2024-02-06 13:50:52 +0530

    modified and made table migration as per new need

---
+++ database/migrations/2024_02_06_081654_create_blogs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->id("id");
+            $table->string("title");
+            $table->string("description");
+            $table->string("is_featured")->default(0);
+            $table->string("tags");
+            $table->unsignedBigInteger("authorId");
+            $table->foreign("authorId")->references("author_id")->on("authors");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('blogs');
+    }
+};
